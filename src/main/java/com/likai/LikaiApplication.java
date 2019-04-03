3b091695b65bd2d3185bbf3ac0a0ae7e532964e9
likai <likai6575@outlook.com>
2019-04-03 17:24:52 +0800

    HelloWorld!

---
+++ src/main/java/com/likai/LikaiApplication.java
@@ -0,0 +1,13 @@
+package com.likai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikaiApplication.class, args);
+    }
+
+}
